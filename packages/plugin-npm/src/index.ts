b616fb62c752c9a0c34dac428f7a9803cb24d46a
Alduino <git@alduino.dev>
2022-04-29 22:14:03 +1000

    Support logging in
    The NPM client asks for a username and an email which aren't required which is kinda annoying, but it isn't the end of the world

--- packages/plugin-npm/src/index.ts
+++ packages/plugin-npm/src/index.ts
@@ -1,12 +1,89 @@
-import {PluginExport} from "@radiantpm/plugin-types";
+import assert from "assert";
+import {createLogger} from "@radiantpm/log";
+import HttpError from "@radiantpm/plugin-error-handler/http-error";
+import {
+    AuthenticationPlugin,
+    DatabasePlugin,
+    PluginExport
+} from "@radiantpm/plugin-types";
+import {
+    createRouteMiddlewarePlugin,
+    RoutedRequestContext
+} from "@radiantpm/plugin-utils";
+import {getJson, setJson} from "@radiantpm/plugin-utils/req-utils";
 
-interface Configuration {
-    /* SET SOME OPTIONS HERE */
+interface CouchLoginBody {
+    name: string;
+    password: string;
 }
 
-const pluginExport: PluginExport<Configuration, false> = {
+let authPlugin: AuthenticationPlugin;
+let dbPlugin: DatabasePlugin;
+
+const logger = createLogger("plugin-npm");
+
+const pluginExport: PluginExport<never, false> = {
+    configIsRequired: false,
+
+    onMetaLoaded(meta) {
+        authPlugin = meta.selectedPlugins.authentication;
+        dbPlugin = meta.selectedPlugins.database;
+    },
+
     init() {
-        return /* CREATE THE PLUGIN HERE */;
+        return [
+            createRouteMiddlewarePlugin(
+                "PUT /-/npm/[feed_slug]/-/user/[user_id]",
+                async (ctx: RoutedRequestContext) => {
+                    const feed = ctx.params.get("feed_slug");
+                    assert(feed, "Missing feed_slug");
+
+                    const {name, password} = await getJson<CouchLoginBody>(
+                        ctx.req
+                    );
+
+                    if (!name || !password) {
+                        logger.trace(
+                            "Invalid request - username or password are missing"
+                        );
+
+                        throw new HttpError(
+                            400,
+                            "Username or password are missing"
+                        );
+                    }
+
+                    // require that the user has access to the feed
+                    const feedAccess = await authPlugin.check(password, {
+                        kind: "feed.view",
+                        slug: feed
+                    });
+
+                    // and check that the feed actually exists
+                    const feedExists = !!(await dbPlugin.getFeedIdFromSlug(
+                        feed
+                    ));
+
+                    if (!feedAccess.success || !feedExists) {
+                        logger.trace(
+                            "Invalid request - either the feed doesn't exist or the user doesn't have access to it"
+                        );
+
+                        throw new HttpError(
+                            404,
+                            "Feed not found, or you may not have access to it"
+                        );
+                    }
+
+                    logger.trace("Login successful");
+
+                    await setJson(ctx.res, 200, {
+                        token: password
+                    });
+                },
+                "npm login"
+            )
+        ];
     }
 };
 
