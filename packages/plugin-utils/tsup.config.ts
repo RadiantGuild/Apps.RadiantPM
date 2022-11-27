40c400bbe4b4323fe53e9d73803ebadee1529a16
Zach Barham <git@alduino.dev>
2022-11-27 21:01:42 +1000

    Use Turborepo to build repository

--- packages/plugin-utils/tsup.config.ts
+++ packages/plugin-utils/tsup.config.ts
@@ -4,7 +4,7 @@ export const tsup: Options = {
     sourcemap: true,
     dts: true,
     clean: true,
-    entryPoints: {
+    entry: {
         "index": "src/index.ts",
         "req-utils": "src/req-utils/index.ts",
         "web": "src/web.ts"
